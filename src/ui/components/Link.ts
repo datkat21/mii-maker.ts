f88c29b3b69ee4a40de27b9ebcd40be61bec2f42
Kat21 <49850194+datkat21@users.noreply.github.com>
2024-10-10 13:35:49 -0500

    Make links not replace open tab

--- src/ui/components/Link.ts
+++ src/ui/components/Link.ts
@@ -1,4 +1,4 @@
 import Html from "@datkat21/html";
 
 export const Link = (name: string, link: string) =>
-  new Html("a").attr({ href: link }).text(name);
+  new Html("a").attr({ href: link, target: "_blank" }).text(name);
